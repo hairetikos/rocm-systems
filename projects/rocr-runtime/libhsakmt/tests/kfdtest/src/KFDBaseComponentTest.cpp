06a07825cc209e89098a38f8de60e8ac58aa466c
Junhua Shen <Junhua.Shen@amd.com>
2025-09-22 13:50:48 +0800

    libhsakmt: Add multi-context test framework support in KFDTest
    - Extend test utilities to enable multi-context testing.
    - Update KFDTest framework to support creation and management of multiple KFD contexts.
    
    Signed-off-by: Junhua Shen <Junhua.Shen@amd.com>

--- projects/rocr-runtime/libhsakmt/tests/kfdtest/src/KFDBaseComponentTest.cpp
+++ projects/rocr-runtime/libhsakmt/tests/kfdtest/src/KFDBaseComponentTest.cpp
@@ -40,7 +40,18 @@ void KFDBaseComponentTest::TearDownTestCase() {
 void KFDBaseComponentTest::SetUp() {
     ROUTINE_START
 
+#ifdef HSAKMT_CTX
+    ASSERT_SUCCESS(hsaKmtOpenKFDCtx(&m_hsakmt_primary_ctx));
+    m_hsakmt_current_ctx = m_hsakmt_primary_ctx;
+
+    #ifdef HSAKMT_SECONDARY_CTX
+        ASSERT_SUCCESS(hsaKmtOpenSecondaryKFDCtx(&m_hsakmt_secondary_ctx));
+        m_hsakmt_current_ctx = m_hsakmt_secondary_ctx;
+    #endif
+#else
     ASSERT_SUCCESS(hsaKmtOpenKFD());
+#endif
+
     EXPECT_SUCCESS(hsaKmtGetVersion(&m_VersionInfo));
     memset( &m_SystemProperties, 0, sizeof(m_SystemProperties) );
     memset(m_RenderNodes, 0, sizeof(m_RenderNodes));
@@ -50,7 +61,7 @@ void KFDBaseComponentTest::SetUp() {
      *  for the rest of the tests and used for more specific topology tests,
      *  call to GetSystemProperties for a system snapshot of the topology here
      */
-    ASSERT_SUCCESS(hsaKmtAcquireSystemProperties(&m_SystemProperties));
+    ASSERT_SUCCESS(HSAKMT_CALL(hsaKmtAcquireSystemProperties, m_hsakmt_current_ctx, &m_SystemProperties));
     ASSERT_GT(m_SystemProperties.NumNodes, HSAuint32(0)) << "HSA has no nodes.";
 
     m_NodeInfo.Init(m_SystemProperties.NumNodes);
@@ -162,8 +173,17 @@ void KFDBaseComponentTest::TearDown() {
         drmClose(m_RenderNodes[i].fd);
     }
 
-    EXPECT_SUCCESS(hsaKmtReleaseSystemProperties());
+#ifdef HSAKMT_CTX
+    #ifdef HSAKMT_SECONDARY_CTX
+        EXPECT_SUCCESS(HSAKMT_CALL(hsaKmtReleaseSystemProperties, m_hsakmt_secondary_ctx));
+        EXPECT_SUCCESS(hsaKmtCloseSecondaryKFDCtx(m_hsakmt_secondary_ctx));
+    #endif
+    EXPECT_SUCCESS(HSAKMT_CALL(hsaKmtReleaseSystemProperties, m_hsakmt_primary_ctx));
+    EXPECT_SUCCESS(hsaKmtCloseKFDCtx());
+#else
+    EXPECT_SUCCESS(HSAKMT_CALL(hsaKmtReleaseSystemProperties, m_hsakmt_current_ctx));
     EXPECT_SUCCESS(hsaKmtCloseKFD());
+#endif
     g_baseTest = NULL;
 
     if (m_pAsm)
@@ -226,7 +246,7 @@ HSAuint64 KFDBaseComponentTest::GetSysMemSize() {
              * Compute total system memory size. KFD driver also computes
              * the system memory (si_meminfo) similarly
              */
-            EXPECT_SUCCESS(hsaKmtGetNodeMemoryProperties(node, 1, &cpuMemoryProps));
+            EXPECT_SUCCESS(HSAKMT_CALL(hsaKmtGetNodeMemoryProperties, m_hsakmt_current_ctx, node, 1, &cpuMemoryProps));
             systemMemSize += cpuMemoryProps.SizeInBytes;
         }
     }
@@ -242,7 +262,7 @@ HSAuint64 KFDBaseComponentTest::GetVramSize(int gpuNode) {
     EXPECT_NE((const HsaNodeProperties *)NULL, nodeProps);
     HSAuint32 numBanks = nodeProps->NumMemoryBanks;
     HsaMemoryProperties memoryProps[numBanks];
-    EXPECT_SUCCESS(hsaKmtGetNodeMemoryProperties(gpuNode, numBanks, memoryProps));
+    EXPECT_SUCCESS(HSAKMT_CALL(hsaKmtGetNodeMemoryProperties, m_hsakmt_current_ctx, gpuNode, numBanks, memoryProps));
     unsigned bank;
     for (bank = 0; bank < numBanks; bank++) {
         if (memoryProps[bank].HeapType == HSA_HEAPTYPE_FRAME_BUFFER_PRIVATE
@@ -310,7 +330,7 @@ int KFDBaseComponentTest::FindDRMRenderNode(int gpuNode) {
 
     nodeProperties = new HsaNodeProperties();
 
-    status = hsaKmtGetNodeProperties(gpuNode, nodeProperties);
+    status = HSAKMT_CALL(hsaKmtGetNodeProperties, m_hsakmt_current_ctx, gpuNode, nodeProperties);
     EXPECT_SUCCESS(status) << "Node index: " << gpuNode << "hsaKmtGetNodeProperties returned status " << status;
 
     if (status != HSAKMT_STATUS_SUCCESS) {
