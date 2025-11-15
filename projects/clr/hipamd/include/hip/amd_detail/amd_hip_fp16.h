17483d29580f1d7214c3706a35de34cf747538e4
Matt Arsenault <Matthew.Arsenault@amd.com>
2025-11-14 20:34:18 -0800

    SWDEV-548892 - Stop using ocml exp10 functions
    Follow exp and exp2. I only separated exp10 just in case
    it breaks a build somewhere, since the elementwise exp10 builtin
    is much more recent than the others.

--- projects/clr/hipamd/include/hip/amd_detail/amd_hip_fp16.h
+++ projects/clr/hipamd/include/hip/amd_detail/amd_hip_fp16.h
@@ -965,7 +965,7 @@ inline __device__ __half hexp2(__half x) {
   return __half_raw{__builtin_elementwise_exp2(static_cast<__half_raw>(x).data)};
 }
 inline __device__ __half hexp10(__half x) {
-  return __half_raw{__ocml_exp10_f16(static_cast<__half_raw>(x).data)};
+  return __half_raw{__builtin_elementwise_exp10(static_cast<__half_raw>(x).data)};
 }
 inline __device__ __half hlog2(__half x) {
   return __half_raw{__ocml_log2_f16(static_cast<__half_raw>(x).data)};
@@ -1014,7 +1014,9 @@ inline __device__ __half2 h2exp(__half2 x) {
 inline __device__ __half2 h2exp2(__half2 x) {
   return __half2{__builtin_elementwise_exp2(static_cast<__half2_raw>(x).data)};
 }
-inline __device__ __half2 h2exp10(__half2 x) { return __half2{__ocml_exp10_2f16(x)}; }
+inline __device__ __half2 h2exp10(__half2 x) {
+  return __half2{__builtin_elementwise_exp10(static_cast<__half2_raw>(x).data)};
+}
 inline __device__ __half2 h2log2(__half2 x) { return __half2{__ocml_log2_2f16(x)}; }
 inline __device__ __half2 h2log(__half2 x) { return __ocml_log_2f16(x); }
 inline __device__ __half2 h2log10(__half2 x) { return __ocml_log10_2f16(x); }
