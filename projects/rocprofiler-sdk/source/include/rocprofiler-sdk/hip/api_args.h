d0a70e68e54ae3578cbd46304ebe97de780f187f
Jaydeep Patel <jaydeepkumar.patel@amd.com>
2025-12-15 09:31:31 +0000

    SWDEV-558839 - Fix formatting in rocprofiler

--- projects/rocprofiler-sdk/source/include/rocprofiler-sdk/hip/api_args.h
+++ projects/rocprofiler-sdk/source/include/rocprofiler-sdk/hip/api_args.h
@@ -3380,14 +3380,14 @@ typedef union rocprofiler_hip_api_args_t
 #if HIP_RUNTIME_API_TABLE_STEP_VERSION >= 21
     struct
     {
-        void**               dev_ptrs;
-        size_t*              sizes;
-        size_t               count;
-        hipMemLocation*      prefetch_locs;
-        size_t*              prefetch_loc_idxs;
-        size_t               num_prefetch_locs;
-        unsigned long long   flags;
-        hipStream_t          stream;
+        void**             dev_ptrs;
+        size_t*            sizes;
+        size_t             count;
+        hipMemLocation*    prefetch_locs;
+        size_t*            prefetch_loc_idxs;
+        size_t             num_prefetch_locs;
+        unsigned long long flags;
+        hipStream_t        stream;
     } hipMemPrefetchBatchAsync;
 #endif
 } rocprofiler_hip_api_args_t;
