d5d466fdc408ab41ae5a7aef1d61823089264302
vlaindic <vladimir.indic@amd.com>
2025-12-24 12:08:31 -0600

    flushing only buffers of the agents used by right clients

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/registration.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/registration.cpp
@@ -697,7 +697,7 @@ invoke_client_detaches()
 
             hsa::async_copy_sync();
             hsa::queue_controller_sync();
-            pc_sampling::service_sync();
+            pc_sampling::service_sync(itr->internal_client_id);
 
             auto _fini_status = get_fini_status();
             if(_fini_status == 0) set_fini_status(-1);
@@ -739,7 +739,7 @@ invoke_client_finalizer(rocprofiler_client_id_t client_id)
 
                 hsa::async_copy_sync();
                 hsa::queue_controller_sync();
-                pc_sampling::service_sync();
+                pc_sampling::service_sync(itr->internal_client_id);
 
                 auto _fini_status = get_fini_status();
                 if(_fini_status == 0) set_fini_status(-1);
