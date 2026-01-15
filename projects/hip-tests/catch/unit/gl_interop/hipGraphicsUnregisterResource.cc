b8fe2a79571ca0e6f19842f5d8324c0aca20f251
jungx098 <jungx098@gmail.com>
2026-01-15 07:19:48 -0800

    SWDEV-575867: Fix hipGraphicsUnregisterResource error checking

--- projects/hip-tests/catch/unit/gl_interop/hipGraphicsUnregisterResource.cc
+++ projects/hip-tests/catch/unit/gl_interop/hipGraphicsUnregisterResource.cc
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+Copyright (c) 2022 - 2026 Advanced Micro Devices, Inc. All rights reserved.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -45,7 +45,7 @@ TEST_CASE("Unit_hipGraphicsUnregisterResource_Negative_Parameters") {
     HIP_CHECK(
         hipGraphicsGLRegisterBuffer(&unregistered_resource, vbo, hipGraphicsRegisterFlagsNone));
     HIP_CHECK(hipGraphicsUnregisterResource(unregistered_resource));
-    HIP_CHECK_ERROR(hipGraphicsUnregisterResource(unregistered_resource), hipErrorInvalidContext);
+    HIP_CHECK_ERROR(hipGraphicsUnregisterResource(unregistered_resource), hipErrorInvalidResourceHandle);
   }
 
   SECTION("mapped resource") {
