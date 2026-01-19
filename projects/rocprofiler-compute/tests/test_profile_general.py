87a6e1c08c8b330005ef1d688f660982d3e421ab
Abhinab Paul Choudhury <abchoudh@amd.com>
2026-01-19 03:18:59 -0600

    Fixed test

--- projects/rocprofiler-compute/tests/test_profile_general.py
+++ projects/rocprofiler-compute/tests/test_profile_general.py
@@ -958,6 +958,7 @@ def test_output_directory(binary_handler_profile_rocprof_compute, monkeypatch):
             binary_handler_profile_rocprof_compute(
                 config, default_workload_dir=workload_base_dir
             )
+            os.chdir("..")
             workload_dir = os.path.join(
                 workload_base_dir,
                 "workloads",
@@ -972,6 +973,7 @@ def test_output_directory(binary_handler_profile_rocprof_compute, monkeypatch):
             binary_handler_profile_rocprof_compute(
                 config, default_workload_dir=workload_base_dir
             )
+            os.chdir("..")
             workload_dir = os.path.join(
                 workload_base_dir,
                 "workloads",
