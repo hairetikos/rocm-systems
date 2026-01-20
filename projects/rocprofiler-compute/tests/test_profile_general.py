b5b3d32d8f5e5f5387094a504681fbd6902bbfed
Abhinab Paul Choudhury <abchoudh@amd.com>
2026-01-20 02:44:18 -0600

    Fixed tests

--- projects/rocprofiler-compute/tests/test_profile_general.py
+++ projects/rocprofiler-compute/tests/test_profile_general.py
@@ -3041,6 +3041,8 @@ def test_multi_rank_profiling():
     workload_base_dir = test_utils.get_output_dir()
 
     num_ranks = 2
+    num_devices = 1
+    num_kernels = 4
 
     baseline_cmd = [
         "mpirun",
@@ -3048,6 +3050,7 @@ def test_multi_rank_profiling():
         str(num_ranks),
         *rocprof_cmd,
         "profile",
+        "--no-roof",
     ]
 
     # Test without MPI-aware workload
@@ -3064,7 +3067,7 @@ def test_multi_rank_profiling():
             workload_dir,
             "--",
         ]
-        + config["occupancy"]
+        + config["app_occupancy"]
     )
     proc = subprocess.run(cmd, text=True, capture_output=True)
     assert proc.returncode == 0
